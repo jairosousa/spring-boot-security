40a8ceafc2f02c3f2733cb8cf0a2d87209523086
jaironsousa1 <jaironsousa@gmail.com>
2019-06-20 13:35:14 -0300

    21. Listando especialidades

--- src/main/java/com/mballem/curso/security/web/controller/EspecialidadeController.java
+++ src/main/java/com/mballem/curso/security/web/controller/EspecialidadeController.java
@@ -3,11 +3,14 @@ package com.mballem.curso.security.web.controller;
 import com.mballem.curso.security.domain.Especialidade;
 import com.mballem.curso.security.service.EspecialidadeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 @RequestMapping("especialidades")
@@ -29,5 +32,12 @@ public class EspecialidadeController {
 
         return "redirect:/especialidades";
     }
+
+    @GetMapping("datatables/server")
+    public ResponseEntity<?> getEspecialidade(HttpServletRequest request) {
+
+        return ResponseEntity.ok(service.buscarEspecialidade(request));
+    }
+
 
 }
