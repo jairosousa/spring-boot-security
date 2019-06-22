7f4277cd2828122ed2136518680c505a9b7c5276
jaironsousa1 <jaironsousa@gmail.com>
2019-06-22 09:31:21 -0300

    36. Métodos salvar e editar para médico

--- src/main/java/com/mballem/curso/security/service/MedicoService.java
+++ src/main/java/com/mballem/curso/security/service/MedicoService.java
@@ -16,4 +16,22 @@ public class MedicoService {
     public Medico buscarPorUsuarioId(Long id) {
         return repository.findByUsuarioId(id).orElse(new Medico());
     }
+
+    @Transactional(readOnly = false)
+    public void salvar(Medico medico) {
+        repository.save(medico);
+    }
+
+    @Transactional(readOnly = false)
+    public void editar(Medico medico) {
+        Medico m2 = repository.findById(medico.getId()).get();
+        m2.setCrm(medico.getCrm());
+        m2.setDtInscricao(medico.getDtInscricao());
+        m2.setNome(medico.getNome());
+        if (!medico.getEspecialidades().isEmpty()){
+            medico.getEspecialidades().addAll(medico.getEspecialidades());
+        }
+
+        repository.save(m2);
+    }
 }
--- src/main/java/com/mballem/curso/security/web/controller/MedicoController.java
+++ src/main/java/com/mballem/curso/security/web/controller/MedicoController.java
@@ -1,19 +1,43 @@
 package com.mballem.curso.security.web.controller;
 
 import com.mballem.curso.security.domain.Medico;
+import com.mballem.curso.security.service.MedicoService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("medicos")
 public class MedicoController {
 
-//    Abrir pagina de dados pessoais de médicos pelo Médico
+    @Autowired
+    private MedicoService service;
+
+    //    Abrir pagina de dados pessoais de médicos pelo Médico
     @GetMapping("dados")
     public String abrirPorMedico(Medico medico, ModelMap model) {
         return "medico/cadastro";
     }
+
+    @GetMapping("salvar")
+    public String salvar(Medico medico, RedirectAttributes attr) {
+        service.salvar(medico);
+        attr.addFlashAttribute("sucesso", "Operação realizada com sucesso");
+        attr.addFlashAttribute("medico", medico);
+        return "redirect:/medicos/dados";
+    }
+
+    @GetMapping("editar")
+    public String editar(Medico medico, RedirectAttributes attr) {
+        service.editar(medico);
+        attr.addFlashAttribute("sucesso", "Operação realizada com sucesso");
+        attr.addFlashAttribute("medico", medico);
+        return "redirect:/medicos/dados";
+    }
+
+
 }
 
