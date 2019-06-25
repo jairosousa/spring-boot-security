524d348862c98f9bf403fbc5f9450a709f5ebc32
jaironsousa1 <jaironsousa@gmail.com>
2019-06-25 19:35:41 -0300

    49 - Inserindo os dados pessoais do paciente

--- src/main/java/com/mballem/curso/security/web/controller/PacienteController.java
+++ src/main/java/com/mballem/curso/security/web/controller/PacienteController.java
@@ -3,12 +3,14 @@ package com.mballem.curso.security.web.controller;
 import com.mballem.curso.security.domain.Paciente;
 import com.mballem.curso.security.domain.Usuario;
 import com.mballem.curso.security.service.PacienteService;
+import com.mballem.curso.security.service.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -18,6 +20,9 @@ public class PacienteController {
     @Autowired
     private PacienteService service;
 
+    @Autowired
+    private UsuarioService usuarioService;
+
     @GetMapping("dados")
     public String cadastrar(Paciente paciente, ModelMap model, @AuthenticationPrincipal User user) {
         paciente = service.buscarPorUsuarioEmail(user.getUsername());
@@ -25,6 +30,19 @@ public class PacienteController {
             paciente.setUsuario(new Usuario(user.getUsername()));
         }
         model.addAttribute("paciente", paciente);
+        return "paciente/cadastro";
+    }
+
+    @PostMapping("salvar")
+    public String salvar(Paciente paciente, ModelMap model, @AuthenticationPrincipal User user) {
+        Usuario u = usuarioService.buscarPorEmail(user.getUsername());
+        if (UsuarioService.isSenhaCorreta(paciente.getUsuario().getSenha(), u.getSenha())) {
+            paciente.setUsuario(u);
+            service.salvar(paciente);
+            model.addAttribute("sucesso", "Seuas dados foram inseridos com sucesso");
+        } else {
+            model.addAttribute("falha", "Sua senha não confere tente novamente");
+        }
         return "paciente/cadastro";
     }
 }
