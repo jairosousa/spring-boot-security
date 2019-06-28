4c984d59ffddd024661a6fff9d92e5b90c4a8111
jaironsousa1 <jaironsousa@gmail.com>
2019-06-28 19:12:43 -0300

    54. Selecionando especialidade via autocomplete

--- src/main/java/com/mballem/curso/security/web/controller/AgendamentoController.java
+++ src/main/java/com/mballem/curso/security/web/controller/AgendamentoController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("agendamento")
+@RequestMapping("agendamentos")
 public class AgendamentoController {
 
     @GetMapping("agendar")
