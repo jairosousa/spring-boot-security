5b1d20bd890bc3c0f855cedf8632edfcf8c0cf7e
jaironsousa1 <jaironsousa@gmail.com>
2019-06-29 09:33:23 -0300

    56. Selecionando a data e hora da consulta

--- src/main/resources/static/js/agendamento.js
+++ src/main/resources/static/js/agendamento.js
@@ -27,26 +27,55 @@ $('#especialidade').on('blur', function() {
     if ( titulo != '' ) {
         $.get( "/medicos/especialidade/titulo/" + titulo , function( result ) {
 
+            if (result.length == 0) {
+                alert("Nenhum médico para essa especialidade no momento")
+                // $("#medicos").append(
+                //     '<div class="custom-control custom-radio">'
+                //     + '<p class="">Nenhum médico para essa especialidade no momento</p>'
+                //     + '</div>')
+            }
+
             var ultimo = result.length - 1;
 
             $.each(result, function (k, v) {
 
-                if ( k == ultimo ) {
-                    $("#medicos").append(
-                        '<div class="custom-control custom-radio">'
-                        +  '<input class="custom-control-input" type="radio" id="customRadio'+ k +'" name="medico.id" value="'+ v.id +'" required>'
-                        +  '<label class="custom-control-label" for="customRadio'+ k +'">'+ v.nome +'</label>'
-                        +  '<div class="invalid-feedback">Médico é obrigatório</div>'
-                        +'</div>'
-                    );
-                } else {
-                    $("#medicos").append(
-                        '<div class="custom-control custom-radio">'
-                        +  '<input class="custom-control-input" type="radio" id="customRadio'+ k +'" name="medico.id" value="'+ v.id +'" required>'
-                        +  '<label class="custom-control-label" for="customRadio'+ k +'">'+ v.nome +'</label>'
-                        +'</div>'
-                    );
-                }
+                    if (k == ultimo) {
+                        $("#medicos").append(
+                            '<div class="custom-control custom-radio">'
+                            + '<input class="custom-control-input" type="radio" id="customRadio' + k + '" name="medico.id" value="' + v.id + '" required>'
+                            + '<label class="custom-control-label" for="customRadio' + k + '">' + v.nome + '</label>'
+                            + '<div class="invalid-feedback">Médico é obrigatório</div>'
+                            + '</div>'
+                        );
+                    } else {
+                        $("#medicos").append(
+                            '<div class="custom-control custom-radio">'
+                            + '<input class="custom-control-input" type="radio" id="customRadio' + k + '" name="medico.id" value="' + v.id + '" required>'
+                            + '<label class="custom-control-label" for="customRadio' + k + '">' + v.nome + '</label>'
+                            + '</div>'
+                        );
+                    }
+            });
+        });
+    }
+});
+
+/**
+ * busca os horários livres para consulta conforme a data e o médico.
+ * os horários são adicionados a página como um select:option.
+ */
+$('#data').on('blur', function () {
+    $("#horarios").empty();
+    var data = $(this).val();
+    var medico = $('input[name="medico.id"]:checked').val();
+    if (!Date.parse(data)) {
+        console.log('data nao selecionada')
+    } else {
+        $.get('/agendamentos/horario/medico/'+ medico + '/data/' + data , function( result ) {
+            $.each(result, function (k, v) {
+                $("#horarios").append(
+                    '<option class="op" value="'+ v.id +'">'+ v.horaMinuto + '</option>'
+                );
             });
         });
     }
